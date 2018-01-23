7906e12bdc4dc1b9a97fccca0d796ea10493384f
liushunlin <liushunlin@zhubajie.com>
2018-01-23 12:45:05 +0800

    添加错误处理

--- public/index.php
+++ public/index.php
@@ -14,4 +14,8 @@ define("BS_URL","cqlaojie.com");
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+try{
+    require __DIR__ . '/../thinkphp/start.php';
+}catch (Exception $e){
+    header('Location:/');
+}
